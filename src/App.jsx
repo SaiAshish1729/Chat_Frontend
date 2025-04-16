cd767305b63bdc6be200c659249eb7db4df3f445
Souvikjr <souvikmiya1996@gmail.com>
2025-04-16 15:49:25 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// import './App.css'
+
+import { Route, Routes } from "react-router-dom"
+import Login from "./pages/authentication/Login"
+import Home from "./pages/home/Home"
+import SignUp from "./pages/authentication/SignUp"
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-red-500'>Hellow world...</h1>
+      <button class="btn btn-active btn-secondary">Secondary</button>
+
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+
+      </Routes>
+
+    </>
+  )
+}
+
+export default App
