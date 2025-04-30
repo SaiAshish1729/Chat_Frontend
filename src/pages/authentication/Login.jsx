5ab246c0d2564dd1a9a0bdfc7ef263c5bd35935e
Ashish <shawashis500@gmail.com>
2025-04-30 21:33:56 +0530

    minor change

--- src/pages/authentication/Login.jsx
+++ src/pages/authentication/Login.jsx
@@ -1,5 +1,4 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import toast from 'react-hot-toast';
 import { FaUser } from "react-icons/fa";
 import { IoKeySharp } from "react-icons/io5";
